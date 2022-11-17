3b9da18c44a3fa84c91faa7ac48e2608a983a683
Kamird <dilmuradovna@gmail.com>
2022-11-16 18:41:00 -0600

    second  commit

--- src/com/java/class27/Example.java
+++ src/com/java/class27/Example.java
@@ -1,2 +1,23 @@
-package com.java.class27;public class Example {
+package com.java.class27;
+
+
+class Car{
+    int numOfCars;
+
+    Car(){
+        numOfCars++;
+
+    }
+}
+public class Example {
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        Car audi = new Car();
+        Car toyota = new Car();
+
+        System.out.println(toyota.numOfCars);
+
+
+    }
+
 }
